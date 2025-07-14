fe245d950ba22ee7047f0496e59f932877637b08
msajiba <asajib499@gmail.com>
2025-07-14 20:17:11 +0600

    Add dynamic export to ensure API route is always dynamic

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic"; // This ensures the route is always dynamic
+
 export const GET = async () => {
   const { data } = await axios.get(
     "https://pqsjapanapi.azurewebsites.net/api/vehicle/getThirdPartyStockListForSEO"
